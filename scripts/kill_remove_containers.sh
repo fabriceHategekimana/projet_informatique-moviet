6f87a19c0295f161e9efbcc5104a3526523c70de
Zenchi <stephane13ste@gmail.com>
2021-03-29 16:57:32 +0200

    added sudo in front of the commands

--- scripts/kill_remove_containers.sh
+++ scripts/kill_remove_containers.sh
@@ -4,12 +4,12 @@ echo "Killing Docker containers and removing them"
 
 # Kill and remove Docker containers
 
-docker kill group-service 2> /dev/null
+sudo docker kill group-service 2> /dev/null
 
 # Add more containers below
 
 
 # Remove docker containers
-docker rm group-service 2> /dev/null
+sudo docker rm group-service 2> /dev/null
 
 # Add more containers below
