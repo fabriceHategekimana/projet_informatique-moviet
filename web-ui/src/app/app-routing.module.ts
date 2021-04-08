e9974a2f5337a7a5200dcde28cfa7039d148619f
Raf <raphael.maggio-aprile@etu.unige.ch>
2021-04-08 23:33:53 +0200

    add error 404

--- web-ui/src/app/app-routing.module.ts
+++ web-ui/src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component'
+import { RegisterComponent } from './register/register.component'
+import { GroupsComponent } from './groups/groups.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent },
+  {path: 'groups', component: GroupsComponent },
   // error 404:
   {path: '**', component: PageNotFoundComponent}
 ];
