7124a6da07ef28a0547a43c07a6323bdb16b756c
Raf <raphael.maggio-aprile@etu.unige.ch>
2021-04-09 00:03:14 +0200

    remove old microservices

--- microservices/counterparty-service/src/main/java/domain/model/STATUS.java
+++
@@ -1,6 +0,0 @@
-package domain.model;
-
-public enum STATUS {
-	ACTIVE,
-	INACTIVE
-}
--- microservices/web-ui/src/app/domain/valuation.ts
+++
@@ -1,8 +0,0 @@
-export class Valuation {
-   breakdownByInstrumentType: [];
-   breakdownByCurrency: [];
-   reportingCurrency: string;
-   currentValue: number;
-   percentile95: number;
-   percentile99: number;
-}
