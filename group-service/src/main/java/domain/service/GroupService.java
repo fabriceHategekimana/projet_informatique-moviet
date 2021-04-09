3654702407601d6c3e03aa94c0c6bfaaed1fa91d
Zenchi <stephane13ste@gmail.com>
2021-04-09 14:58:46 +0200

    Added multiple HTTP requests. PUT : update not yet done. The rest should work. Still no DB

--- group-service/src/main/java/domain/service/GroupService.java
+++ group-service/src/main/java/domain/service/GroupService.java
@@ -4,8 +4,9 @@ import java.util.List;
 import domain.model.Group;
 
 public interface GroupService{
-
-    public List<Group> getAllGroups();
-    public Group getGroup(String id);
-    public void addGroup(String id);
+    public List<Group> getAllGroups(); // called by GET request
+    public Group getGroup(String id); // called by GET request
+    public Group createGroup(Group group); // called by POST request
+    public Group updateGroup(Group group); // called by PUT request
+    public Group deleteGroup(String id); // called by DELETE request
 }
--- group-service/src/test/java/domain/model/GroupTest.java
+++ group-service/src/test/java/domain/model/GroupTest.java
@@ -7,19 +7,28 @@ import org.junit.jupiter.api.Test;
 
 // just a test for unit testing
 public class GroupTest {
-    private final Group group=new Group("1");
+    private final Group group=new Group("1", "hello world");
 
     @Test // if forget to put this : test not launched
     void testGetId(){
         assertEquals("1", group.getId() );
-        //assertEquals(true, true);
     }
 
     @Test
     void testSetId(){
         group.setId("10");
         assertEquals("10", group.getId() );
-        //assertEquals(true, true);
+    }
+
+    @Test
+    void testGetName(){
+        assertEquals("hello world", group.getName() );
+    }
+
+    @Test
+    void testSetName(){
+        group.setName("hello universe");
+        assertEquals("hello universe", group.getName() );
     }
 
     // fail a test -> Build failure
