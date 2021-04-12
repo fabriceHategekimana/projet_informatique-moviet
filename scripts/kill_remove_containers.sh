bf9a03b09e0de4659543d1880f1c243e08267a2a
Zenchi <stephane13ste@gmail.com>
2021-04-12 16:54:08 +0200

    kong-database in kill_remove_containers

--- scripts/kill_remove_containers.sh
+++ scripts/kill_remove_containers.sh
@@ -3,12 +3,12 @@
 echo "Killing Docker containers and removing them"
 
 # Kill and remove Docker containers
-sudo docker kill group-service api-gateway api-gateway-init postgres:10 2> /dev/null
+sudo docker kill group-service api-gateway api-gateway-init kong-database 2> /dev/null
 
 # Add more containers below
 
 
 # Remove docker containers
-sudo docker rm group-service api-gateway api-gateway-init postgres:10 2> /dev/null
+sudo docker rm group-service api-gateway api-gateway-init kong-database 2> /dev/null
 
 # Add more containers below
