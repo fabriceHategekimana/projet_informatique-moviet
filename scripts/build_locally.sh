ae394b197ff72299d7119c4110c4b114b094fe18
Zenchi <stephane13ste@gmail.com>
2021-04-15 22:04:05 +0200

    changed profile name from package-docker-image-with-IT to package-docker-image so that Travis CI build does not fail too

--- scripts/build_locally.sh
+++ scripts/build_locally.sh
@@ -2,6 +2,5 @@
 # Kill and remove docker container first
 sudo scripts/kill_remove_containers.sh
 
-# Build, obtain docker images for microservices and api-gateway, IT are not runned here
-# sudo mvn clean install -Ppackage-docker-image
-sudo mvn clean install -Ppackage-docker-image-with-IT
+# Build, obtain docker images for microservices and api-gateway, docker containers are runned before IT too !
+sudo mvn clean install -Ppackage-docker-image
