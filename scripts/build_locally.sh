c640b3c8cc9b003037f9b0df02cf9699f2570cd3
Zenchi <stephane13ste@gmail.com>
2021-04-16 15:18:48 +0200

    changed back to old profile + skip IT in maven failsafe plugin and removed comment in build_locally about docker containers, they're not runned anymore with this old profile

--- scripts/build_locally.sh
+++ scripts/build_locally.sh
@@ -2,5 +2,5 @@
 # Kill and remove docker container first
 sudo scripts/kill_remove_containers.sh
 
-# Build, obtain docker images for microservices and api-gateway, docker containers are runned before IT too !
+# Build, obtain docker images for microservices and api-gateway
 sudo mvn clean install -Ppackage-docker-image
