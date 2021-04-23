e5f3cf3453e6a949921573c9e9c172c3757a9ecb
Zenchi <stephane13ste@gmail.com>
2021-04-23 18:46:32 +0200

    changed a bit .sql and docker-compose

--- docker-compose/data/groups/groups.sql
+++ docker-compose/data/groups/groups.sql
@@ -4,7 +4,10 @@ create table Group (
     name varchar(255),
     primary key (id)
 );
-
+-- Grant SQL commands: https://www.ibm.com/docs/en/qmf/11.2?topic=privileges-sql-grant-statement
+-- GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public to group-service;
+-- Truncate is here to purge the table without deleting the table: https://sql.sh/cours/truncate-table
+TRUNCATE TABLE GROUP;
 INSERT INTO GROUP (id, name) VALUES ('1', 'erwan');
 INSERT INTO GROUP (id, name) VALUES ('2', 'mohsen');
 INSERT INTO GROUP (id, name) VALUES ('3', 'ethan');
