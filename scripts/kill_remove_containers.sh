3ce636b03f34a7c6291e24d37c72de56a2e929d8
Zenchi <stephane13ste@gmail.com>
2021-04-23 19:25:46 +0200

    docker-compose in scripts. Might have an issue of race condition between the docker-compose commands

--- scripts/kill_remove_containers.sh
+++ scripts/kill_remove_containers.sh
@@ -3,12 +3,7 @@
 echo "Killing Docker containers and removing them"
 
 # Kill and remove Docker containers
-sudo docker kill group-service api-gateway api-gateway-init kong-database 2> /dev/null
-
-# Add more containers below
-
-
-# Remove docker containers
-sudo docker rm group-service api-gateway api-gateway-init kong-database 2> /dev/null
-
-# Add more containers below
+# all microservices
+sudo docker-compose -f docker-compose/docker-compose-microservices.yml down 2> /dev/null
+# api-gw
+sudo docker-compose -f docker-compose/docker-compose-api-gw.yml down 2> /dev/null
