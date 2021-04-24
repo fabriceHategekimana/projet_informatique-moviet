07d0e137605269f4d214c5c177ee3eb86eacc750
Zenchi <stephane13ste@gmail.com>
2021-04-24 17:41:12 +0200

    DB, JPA etc. changes in the REST API too. Probably not working

--- docker-compose/data/groups/groups.sql
+++ docker-compose/data/groups/groups.sql
@@ -1,14 +1,14 @@
-DROP TABLE if exists group_table;
-CREATE TABLE group_table (
-    id varchar(255) not null,
-    name varchar(255),
-    primary key (id)
+DROP TABLE if exists T_groups;
+CREATE TABLE T_groups (
+    id varchar(255) SERIAL PRIMARY KEY,
+    name varchar(255) not null
 );
+-- Auto increment : https://chartio.com/resources/tutorials/how-to-define-an-auto-increment-primary-key-in-postgresql/
 -- Grant SQL commands: https://www.ibm.com/docs/en/qmf/11.2?topic=privileges-sql-grant-statement
 -- GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public to group-service;
 -- Truncate is here to purge the table without deleting the table: https://sql.sh/cours/truncate-table
-TRUNCATE TABLE group_table;
-INSERT INTO group_table (id, name) VALUES ('1', 'erwan');
-INSERT INTO group_table (id, name) VALUES ('2', 'mohsen');
-INSERT INTO group_table (id, name) VALUES ('3', 'ethan');
-INSERT INTO group_table (id, name) VALUES ('42', 'raphael');
+TRUNCATE TABLE T_groups;
+INSERT INTO T_groups (id, name) VALUES ('1', 'erwan');
+INSERT INTO T_groups (id, name) VALUES ('2', 'mohsen');
+INSERT INTO T_groups (id, name) VALUES ('3', 'ethan');
+INSERT INTO T_groups (id, name) VALUES ('42', 'raphael');
--- group-service/src/main/java/domain/service/GroupServiceImpl.java
+++ group-service/src/main/java/domain/service/GroupServiceImpl.java
@@ -9,10 +9,24 @@ import domain.model.Group;
 import javax.enterprise.context.ApplicationScoped; // ApplicationScoped ~singleton
 import lombok.NonNull;
 
+// JPA
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
 @ApplicationScoped
 public class GroupServiceImpl implements GroupService{
     // TODO: DB + be careful about concurrency
     private List<Group> groups=new ArrayList<>(); // temporary, no DB for the moment..
+
+    @PersistenceContext(unitName = "GroupPU") // name is the same as in persistence.xml file
+    private EntityManager em;
+
     /*
     We use null as return when there's an error. The HTTP code associated to them are written in GroupRestService.
     If errors we did not catch, we'll have error 500.. we'll have problems if id's can be null
@@ -21,36 +35,44 @@ public class GroupServiceImpl implements GroupService{
     */
 
     // init list..
+    /*
     public GroupServiceImpl() {
         // https://www.journaldev.com/33297/java-list-add-addall-methods
+        // TODO: remove this
         groups.add(new Group("1", "erwan"));
         groups.add(new Group("2", "mohsen"));
         groups.add(new Group("3", "ethan"));
-        groups.add(new Group("42","raphael"));
+
+
 
     }
+    */
 
     public List<Group> getAllGroups(){
-        return groups;
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Group> criteria = builder.createQuery( Group.class );
+        criteria.from(Group.class);
+        return em.createQuery( criteria ).getResultList();
     }
 
+    // find by ID
     public Group getGroup(@NonNull String id){
-        /* Need to find the group then return it, the code will be replaced later when we'll have DB
-           Moreoever, we suppose id's are unique */
-
-        // https://www.tutorialspoint.com/How-to-iterate-over-a-Java-list
-        // https://crunchify.com/how-to-iterate-through-java-list-4-way-to-iterate-through-loop/
-        for (Group group : groups) {
-            if (group.getId().equals(id)) { // comparison of strings
-                return group;
-            }
-        }
-
-        // if not in the list return null, the Rest Service will take care of returning some HTTP code (404 not found here)
-        return null;
+        /* Need to find the group then return it, Id's are unique
+        if not in the list return null, the Rest Service will take care of returning some HTTP code (404 not found here)
+        https://docs.oracle.com/javaee/7/api/javax/persistence/EntityManager.html#find-java.lang.Class-java.lang.Object-
+        */
+        return em.find(Group.class, id); // null if not found, 404
     }
 
     public Group createGroup(@NonNull Group group){
+        // TODO: verify how id was init ? in Group class. I think we can always create ? because auto increment
+        if (group.getId() != null) {
+            // throw new IllegalArgumentException("Group already exists : " + group.getId());
+            return null; // the Rest Service will take care of returning some HTTP code, here CONFLICT 409
+        }
+        em.persist(group);
+        return group;
+        /*
         // check if group in list of groups
         for (Group g : groups) {
             if (g.getId().equals(group.getId())) { // comparison of strings
@@ -59,9 +81,18 @@ public class GroupServiceImpl implements GroupService{
         }
         groups.add(group);
         return group;
+        */
     }
 
-    public Group updateGroup(@NonNull Group group){ // not working yet
+    public Group updateGroup(@NonNull Group group){
+        Group g = em.find(Group.class, group.getId());
+        if (g == null) {
+            // throw new IllegalArgumentException("Instrument does not exist : " + instrument.getId());
+            return null; // error 404 not found in the group
+        }
+        em.merge(group);
+        return group;
+        /*
         // probably do not want to update the id, only update the name
 
         // find the group first, then update
@@ -70,10 +101,19 @@ public class GroupServiceImpl implements GroupService{
                 return group; // not modifying yet
             }
         }
-        return null; // error 404 not found in the group
+         */
+
     }
 
     public Group deleteGroup(@NonNull String id){
+        Group group = em.find(Group.class, id);
+        if (group == null) {
+            // throw new IllegalArgumentException("Instrument does not exist : " + instrument.getId());
+            return null; // group does not exist, return null -> will be HTTP status code 404 not found
+        }
+        em.remove(group);
+        return group;
+        /*
         for (int index=0; index < groups.size(); index++) {
             if (groups.get(index).getId().equals(id)) { // comparison of strings
                 Group returnedGroup=groups.get(index);
@@ -83,5 +123,6 @@ public class GroupServiceImpl implements GroupService{
         }
         // group does not exist, return null -> will be HTTP status code 404 not found
         return null;
+        */
     }
 }
