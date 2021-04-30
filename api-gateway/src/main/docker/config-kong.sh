33cb3e40f7f743cccd5ac5468ea2a8ac620e70a3
Erwan Don <Erwan.Don@etu.unige.ch>
2021-04-30 15:18:37 +0200

    api last version

--- api-gateway/src/main/docker/config-kong.sh
+++
@@ -1,7 +0,0 @@
-#Creates the services.
-#curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=-service" --data-urlencode "url=http://counterparty-service:8080/counterparties"
-
-#Creates the routes
-#curl -S -s -i -X POST  --url http://api-gateway:8001/services/counterparty-service/routes --data-urlencode "paths[]=/api/v1/counterparty" 
-
-
