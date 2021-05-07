ae5dd566e88c5e8d9be7a38bc9be9df02e49df11
Erwan Don <Erwan.Don@etu.unige.ch>
2021-05-07 14:10:58 +0200

    worked on keycloak (NOT WORKING YET)

--- api-gateway/src/main/docker/config-kong.sh
+++ api-gateway/src/main/docker/config-kong.sh
@@ -5,14 +5,15 @@ curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=group-
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=tmdb-requests-mock" --data-urlencode "url=http://tmdb-requests-service:8080/Mock_movies"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=tmdb-requests-service" --data-urlencode "url=http://tmdb-requests-service:8080/movies"
 
+#creating the routes
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/group-service/routes --data-urlencode "paths[]=/api/v1/groups" 
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/tmdb-requests-mock/routes --data-urlencode "paths[]=/api/v1/Mock_movies"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/tmdb-requests-service/routes --data-urlencode "paths[]=/api/v1/movies"
 
 
 #Enable the Open ID Plugin
-#curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=jwt" --data "enabled=true"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-proxied"   --data "custom_id=api-sso-proxied"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=https://localhost/auth/realms/apigw"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-not-proxied"   --data "custom_id=api-sso-not-proxied"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-not-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=http://iam:8080/auth/realms/apigw"
+curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=jwt" --data "enabled=true"
+curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-proxied"   --data "custom_id=api-sso-proxied"
+curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=https://localhost/auth/realms/apigw"
+curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-not-proxied"   --data "custom_id=api-sso-not-proxied"
+curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-not-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=http://iam:8080/auth/realms/apigw"
