f616ec089a47755f9dc360633a96d101669847a5
Ethan <ethan.icet@etu.unige.ch>
2021-05-07 04:02:43 +0200

    Modified MovieRestService and Mock to introduce the new request getGenres() which return the list of existing genres in TMDb

--- tmdb-requests/src/main/java/api/MovieRestService.java
+++ tmdb-requests/src/main/java/api/MovieRestService.java
@@ -1,5 +1,6 @@
 package api;
 
+import com.uwetrottmann.tmdb2.entities.Genre;
 import domain.model.MovieDisplayInfo;
 import domain.service.MovieRequester;
 
@@ -10,6 +11,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @ApplicationScoped // singleton
 @Path("/movies")
@@ -21,7 +23,7 @@ public class MovieRestService {
     }
 
     @GET
-    @Path("{id}")
+    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getDisplayInfo(@PathParam("id") int id) {
 
@@ -32,6 +34,20 @@ public class MovieRestService {
         }
 
         return Response.ok(displayInfo).build();
+    }
+
+    @GET
+    @Path("/genres")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getGenres() {
+
+        List<Genre> genres = movieRequester.getGenres();
+
+        if (genres == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.ok(genres).build();
     }
 
 }
