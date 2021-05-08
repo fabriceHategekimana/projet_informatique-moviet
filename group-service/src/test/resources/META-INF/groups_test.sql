cd062f852e8c7e218a86ea15109323fb726ff9e7
Zenchi <stephane13ste@gmail.com>
2021-05-08 11:22:51 +0200

    first unit test with h2 in memory db.. need to check the autoincrement id

--- group-service/src/test/resources/META-INF/groups_test.sql
+++ group-service/src/test/resources/META-INF/groups_test.sql
@@ -1,8 +1,6 @@
 DROP TABLE if exists T_groups;
-CREATE TABLE T_groups (
-    id serial primary key,
-    name varchar(255) not null
-);
+-- need to be in one-line otherwise tons of errors..
+CREATE TABLE T_groups (id serial primary key, name varchar(255) not null);
 -- Grant SQL commands: https://www.ibm.com/docs/en/qmf/11.2?topic=privileges-sql-grant-statement
 -- GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA public to group-service;
 -- Truncate is here to purge the table without deleting the table: https://sql.sh/cours/truncate-table
