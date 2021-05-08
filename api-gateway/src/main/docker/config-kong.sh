8d96fb9f75e26dd78e7450b67158c6e0a121ddf2
Ethan <ethan.icet@etu.unige.ch>
2021-05-08 03:41:55 +0200

    Added new requests to Getaway
    Movies signature have been changed to follow an homogenous pattern.
    
    - /Mock_movies => + /movie-service/Mock_movies
    - /movies => + /movie-service/movies
    + /movie-service/search
    + /movie-service/discover

--- api-gateway/src/main/docker/config-kong.sh
+++ api-gateway/src/main/docker/config-kong.sh
@@ -2,14 +2,18 @@
 
 
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=group-service" --data-urlencode "url=http://group-service:8080/groups"
-curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-mock" --data-urlencode "url=http://movie-service:8080/Mock_movies"
-curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service" --data-urlencode "url=http://movie-service:8080/movies"
+curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-movies-mock" --data-urlencode "url=http://movie-service:8080/Mock_movies"
+curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-movies" --data-urlencode "url=http://movie-service:8080/movies"
+curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-search" --data-urlencode "url=http://movie-service:8080/search"
+curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-discover" --data-urlencode "url=http://movie-service:8080/discover"
 
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/group-service/routes --data-urlencode "paths[]=/api/v1/groups" 
-curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-mock/routes --data-urlencode "paths[]=/api/v1/Mock_movies"
-curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service/routes --data-urlencode "paths[]=/api/v1/movies"
+curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-movies-mock/routes --data-urlencode "paths[]=/api/v1/movie-service/Mock_movies"
+curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-movies/routes --data-urlencode "paths[]=/api/v1/movie-service/movies"
+curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-search/routes --data-urlencode "paths[]=/api/v1/movie-service/search"
+curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-discover/routes --data-urlencode "paths[]=/api/v1/movie-service/discover"
 
-
+movie-service/
 #Enable the Open ID Plugin
 #curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=jwt" --data "enabled=true"
 #curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-proxied"   --data "custom_id=api-sso-proxied"
