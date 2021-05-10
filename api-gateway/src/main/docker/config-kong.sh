edb85465fead80f03e57d5465640ea99c8ebb1a5
Erwan Don <Erwan.Don@etu.unige.ch>
2021-05-10 13:38:04 +0200

    google auth (not functionnal yet)

--- api-gateway/src/main/docker/config-kong.sh
+++ api-gateway/src/main/docker/config-kong.sh
@@ -1,19 +1,15 @@
 #Creates the services.
-
-
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=group-service" --data-urlencode "url=http://group-service:8080/groups"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=tmdb-requests-mock" --data-urlencode "url=http://tmdb-requests-service:8080/Mock_movies"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=tmdb-requests-service" --data-urlencode "url=http://tmdb-requests-service:8080/movies"
 
-#creating the routes
+#create the routes
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/group-service/routes --data-urlencode "paths[]=/api/v1/groups" 
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/tmdb-requests-mock/routes --data-urlencode "paths[]=/api/v1/Mock_movies"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/tmdb-requests-service/routes --data-urlencode "paths[]=/api/v1/movies"
 
+#enable the OIDC plugin
+curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=oidc" --data "enabled=true"
 
-#Enable the Open ID Plugin
-curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=jwt" --data "enabled=true"
-curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-proxied"   --data "custom_id=api-sso-proxied"
-curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=https://localhost/auth/realms/apigw"
-curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-not-proxied"   --data "custom_id=api-sso-not-proxied"
-curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-not-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=http://iam:8080/auth/realms/apigw"
+#configure the oidc plugin to use google as idP
+curl -X POST -d 'name=oidc' -d 'config.client_id=858064340266-mti3r0hn9v1dihf9e5vvrs6aini14me6.apps.googleusercontent.com' -d 'config.client_secret =n7es6KYQ5Yrf-j2whND6bhvW' -d 'config.discovery=https://accounts.google.com/.well-known/openid-configuration' http://api-gateway:8001/plugins
