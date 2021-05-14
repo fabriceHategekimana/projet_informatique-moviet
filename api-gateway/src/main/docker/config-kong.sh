546289faa672101635f50c3b1f6a5077abc35a08
Ethan <ethan.icet@etu.unige.ch>
2021-05-14 14:57:32 +0200

    Added Mock_discover to api gateway /api/v1/movie-service/Mock_discover

--- api-gateway/src/main/docker/config-kong.sh
+++ api-gateway/src/main/docker/config-kong.sh
@@ -5,12 +5,14 @@ curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=group-
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-movies-mock" --data-urlencode "url=http://movie-service:8080/Mock_movies"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-movies" --data-urlencode "url=http://movie-service:8080/movies"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-search" --data-urlencode "url=http://movie-service:8080/search"
+curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-discover-mock" --data-urlencode "url=http://movie-service:8080/Mock_discover"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-discover" --data-urlencode "url=http://movie-service:8080/discover"
 
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/group-service/routes --data-urlencode "paths[]=/api/v1/groups" 
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-movies-mock/routes --data-urlencode "paths[]=/api/v1/movie-service/Mock_movies"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-movies/routes --data-urlencode "paths[]=/api/v1/movie-service/movies"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-search/routes --data-urlencode "paths[]=/api/v1/movie-service/search"
+curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-discover-mock/routes --data-urlencode "paths[]=/api/v1/movie-service/Mock_discover"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-discover/routes --data-urlencode "paths[]=/api/v1/movie-service/discover"
 
 movie-service/
