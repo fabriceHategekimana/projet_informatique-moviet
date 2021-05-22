3fd52cd3cf51df329fa4c9d347943858abd38876
Zenchi <stephane13ste@gmail.com>
2021-05-22 18:31:16 +0200

    update whole group including users + addusers seem to work even for new users in t_users or existing users in the same group

--- group-service/src/main/java/domain/model/User.java
+++ group-service/src/main/java/domain/model/User.java
@@ -15,8 +15,6 @@ import javax.validation.constraints.NotNull;
 // https://projectlombok.org/features/all
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Column;
