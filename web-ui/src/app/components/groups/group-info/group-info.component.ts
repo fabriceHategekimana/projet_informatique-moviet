e88ad79c9cba630625a989237d59f4c4aaa1f8ab
Raf <raphael.maggio-aprile@etu.unige.ch>
2021-05-23 18:47:43 +0200

    frontend group user ids

--- web-ui/src/app/components/groups/group-info/group-info.component.ts
+++ web-ui/src/app/components/groups/group-info/group-info.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupsComponent } from '../groups.component'
 import { Group } from '../../../shared/interfaces/group';
+import { User } from '../../../shared/interfaces/user'
+import { UserService } from '../../../services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { fromEventPattern } from 'rxjs';
 
 @Component({
   selector: 'app-group-info',
@@ -11,21 +14,48 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class GroupInfoComponent implements OnInit {
 
   currentGroup?: Group;
+  users: User[] = [];
 
-  constructor(private groupsComponent : GroupsComponent, private router: Router, private route: ActivatedRoute) { }
+  constructor(private groupsComponent : GroupsComponent, private router: Router, private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit(): void {
     this.groupsComponent.getGroup(
       undefined,
       () => {
         this.currentGroup = this.groupsComponent.currentGroup; // save the current group
-        // console.log(this.currentGroup);
+        // import all users:
+        this.getAllUsers();
       }
     )
   }
 
   goToGenres() { // go to the genres selection page
     this.router.navigate(['genres'], {relativeTo: this.route, skipLocationChange: true });
+  }
+
+  // get a single user:
+  getUser(id : number, then: () => any = () => void 0, onError: () => any = () => void 0 ): void {
+    let user: User;
+    this.userService.getUser(Number(id))
+      .subscribe(r => {
+        user = r;
+        if (user) { // if the user exist
+          this.users.push(user);
+          then();
+        } else {
+          onError();
+        }
+      });
+  }
+
+  // get all the users:
+  getAllUsers() {
+    if (this.currentGroup) {
+      for (const groupUser of this.currentGroup.users) {
+        const userId = groupUser.id;
+        this.getUser(userId);
+      } 
+    }
   }
 
 }
