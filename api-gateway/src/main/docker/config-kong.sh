c2f981ac87de02fcb12d851978b405be2fec003d
Erwan Don <Erwan.Don@etu.unige.ch>
2021-05-23 15:37:54 +0200

    oidc is now available on kong

--- api-gateway/src/main/docker/config-kong.sh
+++ api-gateway/src/main/docker/config-kong.sh
@@ -15,10 +15,6 @@ curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-sear
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-search-mock/routes --data-urlencode "paths[]=/api/v1/movie-service/Mock_search"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-discover/routes --data-urlencode "paths[]=/api/v1/movie-service/discover"
 
-movie-service/
-#Enable the Open ID Plugin
-#curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=jwt" --data "enabled=true"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-proxied"   --data "custom_id=api-sso-proxied"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=https://localhost/auth/realms/apigw"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers  --data "username=api-sso-not-proxied"   --data "custom_id=api-sso-not-proxied"
-#curl -S -s -i -X POST  --url http://api-gateway:8001/consumers/api-sso-not-proxied/jwt   -F "algorithm=RS256"  -F "rsa_public_key=@/tmp/keycloak_rsa_provider-key-pub.pem" -F "key=http://iam:8080/auth/realms/apigw"
+#enable the OIDC plugin
+curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=oidc" --data "enabled=true" -d 'config.client_id=CLIENT_ID' -d 'config.client_secret=SECRET' -d 'config.discovery=DISCOVERY_URL'
+
