3d10ffbd16fa8710030b89ec829a79cefcec831b
Erwan Don <Erwan.Don@etu.unige.ch>
2021-05-24 16:41:35 +0200

    reverting changes to use oidg with Ingress

--- api-gateway/src/main/docker/config-kong.sh
+++ api-gateway/src/main/docker/config-kong.sh
@@ -1,4 +1,4 @@
-#Creates the services.
+#Create the services.
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=group-service" --data-urlencode "url=http://group-service:8080/groups"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=user-service" --data-urlencode "url=http://user-service:8080/users"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-movies-mock" --data-urlencode "url=http://movie-service:8080/Mock_movies"
@@ -7,6 +7,7 @@ curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-search-mock" --data-urlencode "url=http://movie-service:8080/Mock_search"
 curl -S -s -i -X POST --url http://api-gateway:8001/services --data "name=movie-service-discover" --data-urlencode "url=http://movie-service:8080/discover"
 
+#Create the routes
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/group-service/routes --data-urlencode "paths[]=/api/v1/groups"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/user-service/routes --data-urlencode "paths[]=/api/v1/users"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-movies-mock/routes --data-urlencode "paths[]=/api/v1/movie-service/Mock_movies"
@@ -15,6 +16,4 @@ curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-sear
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-search-mock/routes --data-urlencode "paths[]=/api/v1/movie-service/Mock_search"
 curl -S -s -i -X POST  --url http://api-gateway:8001/services/movie-service-discover/routes --data-urlencode "paths[]=/api/v1/movie-service/discover"
 
-#enable the OIDC plugin
-curl -S -s -i -X POST  --url http://api-gateway:8001/plugins --data "name=oidc" --data "enabled=true" -d 'config.client_id=CLIENT_ID' -d 'config.client_secret=SECRET' -d 'config.discovery=DISCOVERY_URL'
 
