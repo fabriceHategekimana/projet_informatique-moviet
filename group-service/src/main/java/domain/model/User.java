933b69d82368e240def244f0099a9b21865af700
Zenchi <stephane13ste@gmail.com>
2021-05-28 14:39:18 +0200

    <> added

--- group-service/src/main/java/domain/model/User.java
+++ group-service/src/main/java/domain/model/User.java
@@ -33,7 +33,7 @@ public class User {
 
     @ManyToMany(mappedBy = "users")
     @Setter @JsonIgnore
-    private Set<Group> groups = new HashSet();  // https://www.appsdeveloperblog.com/infinite-recursion-in-objects-with-bidirectional-relationships/
+    private Set<Group> groups = new HashSet<>();  // https://www.appsdeveloperblog.com/infinite-recursion-in-objects-with-bidirectional-relationships/
     // https://thorben-janssen.com/6-hibernate-mappings-you-should-avoid-for-high-performance-applications/
     public User(int id){
         this.id=id;
