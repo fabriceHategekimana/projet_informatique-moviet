3b8c358caa9cf9a54df15e41e7e31b20992da339
Ethan <ethan.icet@etu.unige.ch>
2021-05-30 06:18:12 +0200

    Cleaned some mistakes and incoherence

--- movie-service/src/main/java/domain/service/TmdbConfigurationInterface.java
+++ movie-service/src/main/java/domain/service/TmdbConfigurationInterface.java
@@ -4,12 +4,6 @@ import com.uwetrottmann.tmdb2.Tmdb;
 
 public interface TmdbConfigurationInterface {
 
-    static TmdbConfiguration getInstance() {
-        return null;
-    }
-
-    String getApiKey();
-
     Tmdb getTmdb();
 
     String getBaseUrl();
